749c1056341df9d35e071a57beb30fc0ead51ebe
rjhee <qkdrkwogml@gmail.com>
2021-10-15 23:27:11 +0900

    PWA setting

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,30 @@
+// self.addEventListener('fetch', (event) => {
+//   const req = event.request;
+//   const url = new URL(req.url);
+//   if (url.origin === location.url) {
+//     event.respondWith(cacheFirst(req));
+//   } else {
+//     event.respondWith(newtorkFirst(req));
+//   }
+// });
+
+const _version = 'v1';
+const cacheName = 'v1';
+const cacheList = ['./images/offline.png', './offline.html'];
+
+const log = (msg) => {
+  console.log(`[ServiceWorker${_version}] ${msg}`);
+};
+
+// install
+self.addEventListener('install', (event) => {
+  log('INSTALL');
+});
+
+self.addEventListener('activate', (event) => {
+  log('Activate');
+});
+
 self.addEventListener('fetch', (event) => {
-  const req = event.request;
-  const url = new URL(req.url);
-  if (url.origin === location.url) {
-    event.respondWith(cacheFirst(req));
-  } else {
-    event.respondWith(newtorkFirst(req));
-  }
+  log('Fetch' + event.request.url);
 });
